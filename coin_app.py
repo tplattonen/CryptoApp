4eae1c905b3ea14f5859f419ac14f3b895525e8d
tplattonen <a1704573@myy.haaga-helia.fi>
2019-05-14 21:40:00 +0300

    Added database for storing user info

--- coin_app.py
+++ coin_app.py
@@ -1,11 +1,40 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import registration_form, login_form
 from api_call import coin_json
-app = Flask(__name__)
 
+app = Flask(__name__)
 # Setting a secret key to guard our app against harm
 app.config['SECRET_KEY'] = '78f5f532ab117b6910c29c480eb47b7b'
+# SQLite database to application, and a relative path for it
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///application.db'
+db = SQLAlchemy(app)
 
+# Database structure
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # Relation ship to Post model
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+def __repr__(self):
+    return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+def __repr__(self):
+    return f"Post('{self.title}', '{self.date_posted}', '{self.content}')"
+
+# Coinmarketcap json
 coins = coin_json
 
 #Route for home page
